da4b93e7af3c667b4cdda8e8ae10a77058d72e75
Eddy Filip <eddy.filip@agilebits.com>
2023-07-03 14:52:35 +0100

    Improve code for getting latest CLI version
    There was a duplication of a long and complex command to fetch the latest CLI version (stable or beta). This improvement make it cleaner and clearer that we perform the same step regardless of the release channel.

--- install-cli.sh
+++ install-cli.sh
@@ -3,13 +3,21 @@ set -e
 
 CLI_URL="https://app-updates.agilebits.com/product_history/CLI2"
 
-if [ "$OP_CLI_VERSION" == "latest" ]; then
-    OP_CLI_VERSION="v$(curl -s $CLI_URL | awk -v RS='<h3>|</h3>' 'NR % 2 == 0 {gsub(/[[:blank:]]+/, ""); gsub(/<span[^>]*>|<\/span>|[\r\n]+/, ""); gsub(/&nbsp;.*$/, ""); if (!non_beta && !/beta/){print; non_beta=1;}}')"
-elif [ "$OP_CLI_VERSION" == "latest-beta" ]; then
-    OP_CLI_VERSION="v$(curl -s $CLI_URL | awk -v RS='<h3>|</h3>' 'NR % 2 == 0 {gsub(/[[:blank:]]+/, ""); gsub(/<span[^>]*>|<\/span>|[\r\n]+/, ""); gsub(/&nbsp;.*$/, ""); if (!beta && /beta/){print; beta=1;}}')"
-else
-    OP_CLI_VERSION="v$OP_CLI_VERSION"
-fi
+# Fetch the latest version of 1Password CLI (on stable or beta channel)
+get_latest_cli_version() {
+    conditional_path="/beta/"
+    if [ "$1" == "non_beta" ]; then
+        conditional_path="!/beta/"
+    fi
+    # This long command parses the HTML page at "CLI_URL" and finds
+    # the latest CLI version. It will provide the latest version number
+    # based on the channel we look (stable or beta).
+    #
+    # The ideal call (i.e. 'curl https://app-updates.agilebits.com/check/1/0/CLI2/en/2.0.0/Y -s | jq -r .version')
+    # doesn't retrieve the latest CLI version on a channel basis.
+    # If the latest release is stable and we want the latest beta, this command will return the stable still.
+    OP_CLI_VERSION="v$(curl -s $CLI_URL | awk -v RS='<h3>|</h3>' 'NR % 2 == 0 {gsub(/[[:blank:]]+/, ""); gsub(/<span[^>]*>|<\/span>|[\r\n]+/, ""); gsub(/&nbsp;.*$/, ""); if (!'"$1"' && '"$conditional_path"'){print; '"$1"'=1;}}')"
+}
 
 # Install op-cli
 install_op_cli() {
@@ -30,15 +38,27 @@ install_op_cli() {
             ARCH="arm64"
         fi
 
-        curl -sSfLo op.zip "https://cache.agilebits.com/dist/1P/op2/pkg/$OP_CLI_VERSION/op_linux_amd64_$OP_CLI_VERSION.zip"
+        curl -sSfLo op.zip "https://cache.agilebits.com/dist/1P/op2/pkg/$OP_CLI_VERSION/op_linux_$ARCH_$OP_CLI_VERSION.zip"
         unzip -od "$OP_INSTALL_DIR" op.zip && rm op.zip
     elif [[ "$OSTYPE" == "darwin"* ]]; then
         curl -sSfLo op.pkg "https://cache.agilebits.com/dist/1P/op2/pkg/$OP_CLI_VERSION/op_apple_universal_$OP_CLI_VERSION.pkg"
         pkgutil --expand op.pkg temp-pkg
         tar -xvf temp-pkg/op.pkg/Payload -C "$OP_INSTALL_DIR"
         rm -rf temp-pkg && rm op.pkg
+    else
+        echo "Install 1Password CLI GitHub Action isn't supported on this operating system yet: $OSTYPE."
     fi
     echo "$OP_INSTALL_DIR" >>"$GITHUB_PATH"
 }
+
+# Main action of the script
+
+if [ "$OP_CLI_VERSION" == "latest" ]; then
+    get_latest_cli_version non_beta
+elif [ "$OP_CLI_VERSION" == "latest-beta" ]; then
+    get_latest_cli_version beta
+else
+    OP_CLI_VERSION="v$OP_CLI_VERSION"
+fi
 
 install_op_cli
--- test/assert-version.sh
+++ test/assert-version.sh
@@ -4,10 +4,27 @@ set -e
 OP_CLI_VERSION="$1"
 CLI_URL="https://app-updates.agilebits.com/product_history/CLI2"
 
+get_latest_cli_version() {
+    conditional_path="/beta/"
+    if [ "$1" == "non_beta" ]; then
+        conditional_path="!/beta/"
+    fi
+    # This long command parses the HTML page at "CLI_URL" and finds
+    # the latest CLI version. It will provide the latest version number
+    # based on the channel we look (stable or beta).
+    #
+    # The ideal call (i.e. 'curl https://app-updates.agilebits.com/check/1/0/CLI2/en/2.0.0/Y -s | jq -r .version')
+    # doesn't retrieve the latest CLI version on a channel basis.
+    # If the latest release is stable and we want the latest beta, this command will return the stable still.
+    OP_CLI_VERSION="v$(curl -s $CLI_URL | awk -v RS='<h3>|</h3>' 'NR % 2 == 0 {gsub(/[[:blank:]]+/, ""); gsub(/<span[^>]*>|<\/span>|[\r\n]+/, ""); gsub(/&nbsp;.*$/, ""); if (!'"$1"' && '"$conditional_path"'){print; '"$1"'=1;}}')"
+}
+
 if [ "$OP_CLI_VERSION" == "latest" ]; then
-    OP_CLI_VERSION="$(curl -s $CLI_URL | awk -v RS='<h3>|</h3>' 'NR % 2 == 0 {gsub(/[[:blank:]]+/, ""); gsub(/<span[^>]*>|<\/span>|[\r\n]+/, ""); gsub(/&nbsp;.*$/, ""); if (!non_beta && !/beta/){print; non_beta=1;}}')"
+    get_latest_cli_version non_beta
 elif [ "$OP_CLI_VERSION" == "latest-beta" ]; then
-    OP_CLI_VERSION="$(curl -s $CLI_URL | awk -v RS='<h3>|</h3>' 'NR % 2 == 0 {gsub(/[[:blank:]]+/, ""); gsub(/<span[^>]*>|<\/span>|[\r\n]+/, ""); gsub(/&nbsp;.*$/, ""); if (!beta && /beta/){print; beta=1;}}')"
+    get_latest_cli_version beta
+else
+    OP_CLI_VERSION="v$OP_CLI_VERSION"
 fi
 
 if [ "$(op --version)" != "$OP_CLI_VERSION" ]; then
