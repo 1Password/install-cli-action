f8fe031a62ea02abd840441cc41381683810c98c
Volodymyr Zotov <volodymyr.zotov@agilebits.com>
2025-08-04 22:43:16 -0500

    Check formatting and lint on pre-commit/pre-push steps
    If failed, it should not push had users have to fix formatting and eslint errors by himself and re-try to commit/push

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,7 +1,7 @@
 const lintStagedConfig = {
 	// run formatting and linting on all supported file types
-	"*.{js,json,md,ts,yaml,yml}": "npm run format:write",
-	"*.{js,ts}": ["npm run lint:fix"],
+	"*.{js,json,md,ts,yaml,yml}": "npm run format:check",
+	"*.{js,ts}": ["npm run lint"],
 };
 
 export default lintStagedConfig;
